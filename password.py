e45eadf9707ddb87c44eba4d54e3e777bb89d85e
CouchCouch <68971566+CouchCouch@users.noreply.github.com>
2021-10-26 18:24:23 -0400

    save

--- password.py
+++ password.py
@@ -2,4 +2,8 @@ import random
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 numbers = '1234567890'
-symbols = "`~!@#$%^&*()-_=+\|]}[{\""
+symbols = "`~!@#$%^&*()-_=+\|]}[{\"';:,.<>?//"
+
+print("Length of password?\n")
+while(i=1, i==1):
+    print(True)
