354420a0b728e8a4c19911528ec76f98c0044aaf
Ryan Couchman <68971566+CouchCouch@users.noreply.github.com>
2021-10-26 22:45:16 +0000

    finished the password generator

--- password.py
+++ password.py
@@ -3,7 +3,16 @@ import random
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 numbers = '1234567890'
 symbols = "`~!@#$%^&*()-_=+\|]}[{\"';:,.<>?//"
-
-print("Length of password?\n")
-while(i=1, i==1):
-    print(True)
+password = ""
+print("Length of password?")
+length = input()
+for i in range(int(length)):
+  if i%5 == 1:
+    password += random.choice(symbols)
+  elif i%4 == 1:
+    password += random.choice(numbers)
+  elif i%2 == 1:
+    password += random.choice(alphabet)
+  else:
+    password += random.choice(alphabet)
+print("Your password is: ", password)
